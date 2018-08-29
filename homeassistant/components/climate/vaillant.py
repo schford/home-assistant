859f9bdd1adf080c0472dcecab75f088a238a498
schford <34009231+schford@users.noreply.github.com>
2018-08-29 20:30:57 +0100

    Update vaillant.py
    Tweaked so that Card shows correct state of device.

--- homeassistant/components/climate/vaillant.py
+++ homeassistant/components/climate/vaillant.py
@@ -8,7 +8,8 @@ https://home-assistant.io/components/climate.netatmo/
 import logging
 from datetime import timedelta
 import voluptuous as vol
-from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE
+from homeassistant.const import (
+     TEMP_CELSIUS, ATTR_TEMPERATURE)
 from homeassistant.components.climate import (
     STATE_HEAT, STATE_IDLE, ClimateDevice, PLATFORM_SCHEMA,
     SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE, SUPPORT_AWAY_MODE)
@@ -70,24 +71,19 @@ class NetatmoThermostat(ClimateDevice):
         self._away = None
 
     @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
     def supported_features(self):
         """Return the list of supported features."""
         return SUPPORT_FLAGS
 
     @property
-    def state(self):
-        """Return the state of the device."""
-        return self._target_temperature
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
 
     @property
     def temperature_unit(self):
         """Return the unit of measurement."""
-        return TEMP_CELSIUS
+        return TEMP_CELSIUS		
 
     @property
     def current_temperature(self):
